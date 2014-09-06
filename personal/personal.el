efb545a0c08992a6e89f77c5178a3b9c144db615
eapolinario <curupa@gmail.com>
2014-09-05 18:50:25 -0700

    Adding first parts of personal.el

---
+++ personal/personal.el
@@ -0,0 +1,14 @@
+;; Window move
+(global-set-key (kbd "C-x <up>") 'windmove-up)
+(global-set-key (kbd "C-x <down>") 'windmove-down)
+(global-set-key (kbd "C-x <right>") 'windmove-right)
+(global-set-key (kbd "C-x <left>") 'windmove-left)
+
+;; Enable forward/backward word on OSX
+(global-set-key '[(meta right)] 'forward-word)
+(global-set-key '[(meta left)] 'backward-word)
+
+;; "cat -vt" is your friend
+;; (define-key input-decode-map "\e[1;3C" [(meta right)])
+;; (define-key input-decode-map "\e[1;3D" [(meta left)])
+
